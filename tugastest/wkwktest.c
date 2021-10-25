cd4511df9f92acf83f953e7f7ce185c412e4bb87
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2021-10-25 07:46:18 +0700

    test

--- tugastest/wkwktest.c
+++ tugastest/wkwktest.c
@@ -1,3 +1,13 @@
 #include<stdio.h>
-#include"wkwk.h"
-//test dynamic array
+
+float kala(int p, int l){
+    int luas=p+l;
+    return luas;
+}
+
+int main(){
+    int o,u;
+    o=12;
+    u=123;
+    printf("hasil : %f",kala(o,u));
+}

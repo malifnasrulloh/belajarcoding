fcb56e38fed2b125537517c51e20a5cd9b541af2
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2021-11-16 01:14:33 +0700

    java

---
+++ java/2print/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World!");
+        System.out.print("Hello world");
+        System.out.print(" PPPPP\n");
+        System.out.printf("Hallo %d",1000);
+
+    }
+}
---
+++ java/3string[]args/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){ //string[] args untuk menampung nilai setelah pemanggilan pada jalan kan program
+        // args[0]="Hallo";
+        System.out.println(args[0]); //contoh saat jalan kan program java Main.java hallo, maka hallo akan di print
+    }
+}

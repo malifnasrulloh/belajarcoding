42599ad7080b961c92dc8e7bf871c1aed023f6a7
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2021-11-22 20:29:03 +0700

    java

---
+++ java/7operatorunary/src/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args){
+        int a=12;
+        System.out.printf("a : %d",-a);
+        System.out.printf("\na : %d",+a);
+
+        a=-12;
+        System.out.printf("\na : %d",-a);
+        System.out.printf("\na : %d",+a);
+        System.out.printf("\nHello world");
+
+        //operator komparasi
+        System.out.printf("\n%d sama dengan %d ? : %s",-12,12,a==-12);
+        System.out.printf("\n%d sama dengan %d ? : %s",12,11,a==11);
+        if (a==1) {
+            System.out.printf("\n%d sama dengan %d ? : %s",12,12,(a==12));
+        }else if(a==1){
+            System.out.printf("\n%d sama dengan %d ? : %s",12,12,(a==12));
+        }
+        switch (a) {
+            case 1:
+                System.out.printf("\n%d sama dengan %d ? : %s",12,12,(a==12));
+                break;
+            default:
+                break;
+        }
+    }
+}

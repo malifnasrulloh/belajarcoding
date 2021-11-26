f5b2b4ecabcc0b9c4588a3d8611a5c6688051b4e
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2021-11-27 00:31:50 +0700

    java

--- java/8operatorbitwise/src/Main.java
+++ java/8operatorbitwise/src/Main.java
@@ -2,12 +2,34 @@
 
 public class Main {
     // static Scanner scanf=new Scanner(System.in);
+    /*
+    and             &
+    or              |
+    xor             ^
+    not             ~
+    shift left      <<
+    ----- right     >>
+    */
     public static void main(String[] args){
-        bit(0);
-        bit(1);
+        bit(4);
+        bit(7);
+        bit('&',4&7);
+        bit('|',4|7);
+        bit('^',4^7);
+        bit('~',~4);
+        bit("<<",9<<2);
+        bit(">>",9>>2);
     }
     static void bit(int b){
         String bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
-        System.out.printf("binary dari %d : %s\n",b,bits);
+        System.out.printf("binary dari %d\t\t: %s\n",b,bits);
+    }
+    static void bit(String wise,int b){
+        String bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("bitwise (%s) menjadi %d\t: %s\n",wise,b,bits);
+    }
+    static void bit(char wise,int b){
+        String bits = String.format("%8s",Integer.toBinaryString(b)).replace(' ','0');
+        System.out.printf("bitwise (%s) menjadi %d\t: %s\n",wise,b,bits);
     }
 }

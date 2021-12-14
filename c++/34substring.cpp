889d08d637c1c9809bd9483431b9ceefe1884389
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2021-12-14 14:49:37 +0700

    kuliah

---
+++ c++/34substring.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string nama="Muhammad Alif Nasrulloh";
+    cout<< nama.substr(9,4)<<endl;
+
+    cout<<nama.find("Alif");
+}
---
+++ tugastest/dina1412.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class dina1412{
+	public static void main(String[] args){
+		int pilih;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Menu");
+        System.out.println("1. Data Siswa");
+        System.out.println("2. Mata Pelajaran");
+        System.out.println("3. Input Nilai Siswa");
+        System.out.println("4. Rapor Nilai");
+        System.out.println("5. Nilai per Mata Pelajaran");
+        System.out.print("\nPilih : ");
+        pilih=input.nextInt();
+
+        if(pilih == 1){
+            System.out.println("\nMenampilkan Data Siswa");
+        }else if(pilih == 2){
+            System.out.println("\nMenampilkan Mata Pelajaran");
+        }else if(pilih == 3){
+            System.out.println("\nMenampilkan Input Nilai Siswa");
+        }else if(pilih == 4){
+            System.out.println("\nMenampilkan Rapor Nilai");
+        }else if(pilih == 5){
+            System.out.println("\nMenampilkan Nilai per Mata Pelajaran");
+        }
+        System.out.println("\n\n===== Data Siswa =====");
+        System.out.println("Nama\t\t: Dina Maulidatur Rahma");
+        System.out.println("No. Absen\t: 10");
+        System.out.println("Kelas\t\t: 11 MIPA 4");
+
+
+        System.out.println("\n\nMata Pelajaran\t  Nilai Siswa");
+        System.out.println("-----------------------------");
+        String[] maPel = {"Matematika","Biologi\t","Fisika\t","Kimia\t","B.Inggris","B.Indonesia","Agama\t","PKn\t","Sejarah\t"};
+        int[] nilai = {90,95,90,91,93,94,89,90,88};
+        for(int i = 0; i<nilai.length; i++){
+            System.out.println(maPel[i]+"\t|\t"+nilai[i]);
+        }
+	}
+}

d9d6cc6bab54525f60ef4d288dd4a0d435027d5e
malifnasrulloh <alifnasrulloh.jbg@gmail.com>
2022-09-17 11:29:38 +0700

    java

--- enc.java
+++ enc.java
@@ -1,6 +1,3 @@
-import java.lang.invoke.MethodHandles.Lookup;
-import java.util.Random;
-
 public class enc{
     static String[] ascii={"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
     static StringBuilder temp;
